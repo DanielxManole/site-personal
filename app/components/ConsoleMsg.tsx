45d76abda039008c57879a698b4cf1a99dcddf9d
Manole Daniel <168420032+DanielxManole@users.noreply.github.com>
2025-12-04 19:36:32 +0200

    Update ConsoleMsg.tsx

--- app/components/ConsoleMsg.tsx
+++ app/components/ConsoleMsg.tsx
@@ -6,8 +6,8 @@ export default function ConsoleHello() {
   useEffect(() => {
     const timer = setTimeout(() => {
       console.log(
-        "%c SYSTEM_READY %c \n\nSalut! Contactează-mă pentru o colaborare: manoledaniel2004@gmail.com",
-        "background: #2563eb; color: white; padding: 4px; border-radius: 4px; font-weight: bold;",
+        "\n%c SYSTEM_READY %c \n\nSalut! Contactează-mă pentru o colaborare: manoledaniel2004@gmail.com",
+        "background: #2563eb; color: white; padding: 10px; border-radius: 4px; font-weight: bold;",
         "color: #F00;"
       );
     }, 2000);
